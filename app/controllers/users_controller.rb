23e0a0831496525fac8a3d9ef1c58e28cb3edecd
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2016-10-29 19:11:26 +0900

    Complete making User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,16 +29,18 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    if params[:user][:image]
+      @user.image = params[:user][:image].read # <= バイナリをセット
+      @user.data_type = params[:user][:image].content_type # <= ファイルタイプをセット
+    end
     respond_to do |format|
       if @user.save
-         @user.image = params[:user][:image].read # <= バイナリをセット
-         @user.data_type = params[:user][:image].content_type # <= ファイルタイプをセット
-         @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+
+       format.html { redirect_to @user, notice: 'User was successfully created.' }
+       format.json { render :show, status: :created, location: @user }
+     else
+      format.html { render :new }
+      format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -84,6 +86,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :account, :password, :password_confirmation,:image,:data_type)
+      params.require(:user).permit(:name, :account, :image, :data_type, :password,:password_confirmation)
     end
   end
